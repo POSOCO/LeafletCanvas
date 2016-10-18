549293d582aca64617fa8bd6949229ba4285b7c8
nagasudhirpulla <nagasudhirpulla@gmail.com>
2016-10-18 21:26:54 +0530

    completed voltage contour

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LeafletCanvas
 Data Visualization with Leaflet Library
+
+##Todos
+1. Label layer that displays the names of cities and substations should be above the visualization layer. So the layers order should be map tiles, visualization layer, label layer, marker layer
+2. Load different tiles that can load various useful information like real time weather data onto the map
---
+++ libs/rgbhsv.js
@@ -0,0 +1,95 @@
+/*
+ * HSV to RGB color conversion
+ *
+ * H runs from 0 to 360 degrees
+ * S and V run from 0 to 1
+ *
+ * Ported from the excellent java algorithm by Eugene Vishnevsky at:
+ * http://www.cs.rit.edu/~ncs/color/t_convert.html
+ */
+function hsvToRgb(h, s, v) {
+    var r, g, b;
+    var i;
+    var f, p, q, t;
+    // Make sure our arguments stay in-range
+    h = Math.max(0, Math.min(360, h));
+    ////s = Math.max(0, Math.min(1, s));
+    ////v = Math.max(0, Math.min(1, v));
+    /****if(s == 0) {
+			// Achromatic (grey)
+			r = g = b = v;
+			return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+			}****/
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
+    switch (i) {
+        case 0:
+            r = v;
+            g = t;
+            b = p;
+            break;
+        case 1:
+            r = q;
+            g = v;
+            b = p;
+            break;
+        case 2:
+            r = p;
+            g = v;
+            b = t;
+            break;
+        case 3:
+            r = p;
+            g = q;
+            b = v;
+            break;
+        case 4:
+            r = t;
+            g = p;
+            b = v;
+            break;
+        default: // case 5:
+            r = v;
+            g = p;
+            b = q;
+    }
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+/*
+ * RGB to HSV color conversion
+ http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+ */
+function rgbToHsv(r, g, b) {
+    r = r / 255;
+    g = g / 255;
+    b = b / 255;
+    var max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    var h, s, v = max;
+
+    var d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if (max == min) {
+        h = 0; // achromatic
+    } else {
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+        h *= 60;
+    }
+    return [h, s, v];
+}
